45ba784ee38c010dec39b14cb862398740078093
Patrick Mwangi <frashid274@gmail.com>
2025-05-26 16:32:16 +0300

    feat: implement routing for main and authentication pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Route, Switch } from 'wouter';
+import './index.css';
+
+import App from './App.jsx';
+import AuthPage from './pages/auth-page.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Switch>
+      <Route path="/" component={App} />
+      <Route path="/auth" component={AuthPage} />
+      <Route>404: Page not found</Route>
+    </Switch>
+  </StrictMode>
+);
